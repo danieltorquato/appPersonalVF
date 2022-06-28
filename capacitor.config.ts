10b6d9263860bdd8221ef1ef74f0ea5fa92f6ea5
Daniel Monteiro da Silva Torquato <danieltorquato2009@gmail.com>
2022-06-28 00:36:48 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.com.torquatoti',
   appName: 'VitorF',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)
+  },
+  {
+    path: 'schedule',
+    loadChildren: () => import('./schedule/schedule.module').then( m => m.SchedulePageModule)
+  },
+  {
+    path: 'hours',
+    loadChildren: () => import('./hours/hours.module').then( m => m.HoursPageModule)
   }
 ];
 @NgModule({
