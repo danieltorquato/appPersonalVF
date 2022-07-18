a7f60032d9ebde4d956e6c0f3d5bf84a00501c1a
danieltorquato <danieltorquato2009@gmail.com>
2022-07-17 21:53:20 -0300

    att

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,17 @@ export const environment = {
   production: false,
 };
 export const firebaseConfig = {
-apiKey: 'AIzaSyAis-ogZ0l8GygktNk95ueFMLzCIYyiGrw',
-authDomain: 'vitorf-2a488.firebaseapp.com',
-databaseURL: 'https://vitorf-2a488-default-rtdb.firebaseio.com',
-projectId: 'vitorf-2a488',
-storageBucket: 'vitorf-2a488.appspot.com',
-  messagingSenderId: '811667284264',
-  appId: '1:811667284264:web:959e31827844a63bad301d',
-  measurementId: 'G-KCJ87123ST',
+
+apiKey: '',
+authDomain: '',
+databaseURL: '',
+projectId: '',
+storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
