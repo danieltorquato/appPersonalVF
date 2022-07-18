db65e14716da188cccdf05a8b8b1e62e46c940b0
danieltorquato <danieltorquato2009@gmail.com>
2022-07-17 21:47:11 -0300

    att

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
 };
@@ -13,7 +12,7 @@ projectId: 'vitorf-2a488',
 storageBucket: 'vitorf-2a488.appspot.com',
   messagingSenderId: '811667284264',
   appId: '1:811667284264:web:959e31827844a63bad301d',
-  measurementId: 'G-KCJ87123ST'
+  measurementId: 'G-KCJ87123ST',
 };
 /*
  * For easier debugging in development mode, you can import the following file
